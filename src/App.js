cfb0b1e463593a1931c926605285b445907f377d
raj <raj.anand0511@gmail.com>
2022-02-02 16:17:58 +0530

    meme generator frontend

--- src/App.js
+++ src/App.js
@@ -1,23 +1,62 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from "react";
+import "./styles/App.css";
+import "axios";
+import { getData } from "./api/getMemes";
+import MemeContainer from "./component/memeContainer";
+
 
 function App() {
+  const [memes, setMemes] = useState();
+  const [index, setIndex] = useState(Math.floor(Math.random() * 100));
+
+  useEffect(() => {
+    if (memes === undefined)
+      getData().then((res) => {
+        setMemes(res);
+        console.log(res);
+      });
+  });
+
+  const goPrevious = () => {
+    let newIndex = (index - 1) % 100;
+    if (newIndex < 0) newIndex = 99;
+    setIndex(newIndex);
+  };
+
+  const goNext = () => {
+    let newIndex = (index + 1) % 100;
+    setIndex(newIndex);
+  };
+
+  const getRandom = () => {
+    setIndex(Math.floor(Math.random() * 100));
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <h1>Meme Generator</h1>
+
+      <MemeContainer
+        url={
+          memes === undefined
+            ? "https://i.imgflip.com/4/30b1gx.jpg"
+            : memes[index].url
+        }
+        title={memes === undefined ? "Drake meme" : memes[index].name}
+        
+      />
+
+      <div className="app__memeSelector">
+        <div className="app__memeSelector__button" onClick={() => goPrevious()}>
+          Go Previous
+        </div>
+        <div className="app__memeSelector__button" onClick={() => getRandom()}>
+          Get Random
+        </div>
+        <div className="app__memeSelector__button" onClick={() => goNext()}>
+          Go Next
+        </div>
+      </div>
     </div>
   );
 }
